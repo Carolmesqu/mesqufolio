0af25bc3f191a6ea3fd3f0d55c5e3535b1a59b2d
Maria <carolmesqu@gmail.com>
2025-03-21 21:42:39 -0300

    feat: add Menu component and integrate it into the header; create project detail page

---
+++ frontend/src/app/(paginas)/projeto/[id]/page.tsx
@@ -0,0 +1,3 @@
+export default function PaginaProjeto() {
+    return <div>Detalhes do Projeto</div>
+}
--- frontend/src/app/components/shared/Menu.tsx
+++ frontend/src/app/components/shared/Menu.tsx
@@ -1,3 +1,32 @@
-export default function Menu() { 
+
+"use client"
+import { usePathname } from "next/navigation"
+import Link from "next/link"
+
+export default function Menu() {
+    const caminho = usePathname()
     
+    return (
+        <nav className="flex gap-6">
+            <MenuItem href="/" selecionado={caminho === "/"}>Início</MenuItem>
+            <MenuItem href="/projeto/1" selecionado={caminho === "/projeto"}>Projetos</MenuItem>
+            <MenuItem href="https://api.whatsapp.com/send/?phone=5521988171853&text&type=phone_number" selecionado={false} novaAba={true}>Contato</MenuItem>
+        </nav>
+    )
+}
+
+function MenuItem(props: { 
+    href: string
+    children: React.ReactNode
+    selecionado: boolean
+    novaAba?: boolean
+}) { 
+    return (        
+        <Link href={props.href} target={props.novaAba ? "_blank" : "_self"}>
+            <span className={`
+                flex item-center gap-2 text-sm border-green-600 hover:text-gray-400
+                ${props.selecionado ? "border-b-4 text-write" : "text-zinc-300"}
+            `}>{props.children}</span>
+        </Link>
+    )
 }
