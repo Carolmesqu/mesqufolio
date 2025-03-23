bd1b6aadd61d2d94b9bd99eef3a61e67d240b799
Maria <carolmesqu@gmail.com>
2025-03-23 15:32:45 -0300

    feat: add utility functions and components for improved styling and layout

--- frontend/src/app/components/projetos/ItemProjeto.tsx
+++ frontend/src/app/components/projetos/ItemProjeto.tsx
@@ -7,7 +7,7 @@ export interface ItemProjetoProps {
 }
 
 export default function ItemProjeto(props: ItemProjetoProps) {
-    return <Link href={`/projetos/${props.projeto.id}`}>
+    return <Link href={`/projeto/${props.projeto.id}`}>
         <div className="relative rounded-2xl overflow-hidden border border-zinc-800 min-w-64 min-h-64 ">
             <Image 
                 src={props.projeto.imagens[0]} 
