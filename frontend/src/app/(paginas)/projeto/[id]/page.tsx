3e2b0b65090488986810e01cb91dc1e016cbe679
Maria <carolmesqu@gmail.com>
2025-03-23 18:01:31 -0300

    feat: fetch and display README content in PaginaProjeto

--- frontend/src/app/(paginas)/projeto/[id]/page.tsx
+++ frontend/src/app/(paginas)/projeto/[id]/page.tsx
@@ -2,11 +2,17 @@ import Cabecalho from "@/app/components/shared/Cabecalho";
 import CarrosselImagens from "@/app/components/shared/CarrosselImagens";
 import Container from "@/app/components/shared/Container";
 import Tecnologias from "@/app/components/tecnologias/Tecnologias";
+import { obterReadme } from "@/functions/github";
 import { obterProjeto } from "@/functions/projetos";
 
 export default async function PaginaProjeto(props: { params: Promise<{ id: string }> }) {
     const { id } = await props.params;
     const projeto = await obterProjeto(id);
+
+    if(!projeto) return null
+
+    const readme = await obterReadme(projeto.repositorio)
+
     return projeto ? (
         <div className="bg-black">
             <Cabecalho />
@@ -16,5 +22,5 @@ export default async function PaginaProjeto(props: { params: Promise<{ id: strin
                 <Tecnologias lista={projeto.tecnologias} tamanhoMenor/>
             </Container>         
         </div>
-    ) : null
+    )
 }
